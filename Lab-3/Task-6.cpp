78d1a5a52873fb9a3d4a4f882e7cdc6cd4a77cca
Mohammad Farhad(2017831008) <wahedfarhad123@gmail.com>
2019-07-21 22:47:41 +0600

    Add files via upload

---
+++ Lab-3/Task-6.cpp
@@ -0,0 +1,62 @@
+
+//Hashing Quadratic Probing
+
+#include<bits/stdc++.h>
+using namespace std;
+int tsize;
+
+int funct1(int key)
+{
+    int i ;
+    i = key%tsize ;
+    return i;
+}
+
+int funct2(int key, int j)
+{
+    int i ;
+    i = (key+(j*j))%tsize ;
+    return i ;
+}
+
+int main()
+{
+    int key,arr[100],hash[100],i,n,j;
+    printf ("Enter the size of the hash table:  ");
+    cin>>tsize;
+
+    printf ("Enter the number of elements: ");
+    cin>>n;
+
+    for (int i=0; i<tsize; i++)
+        hash[i]=-1 ;
+
+    printf ("Enter Elements: ");
+    for (int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    for (int i=0; i<tsize; i++)
+        hash[i]=-1 ;
+
+    for(int k=0; k<n; k++)
+    {
+        j=1;
+        key=arr[k] ;
+        i = funct1(key);
+        while (hash[i]!=-1)
+        {
+            i = funct2(i,j);
+            j++ ;
+        }
+        hash[i]=key ;
+    }
+    printf("The elements in the array are: ");
+    for (i=0; i<tsize; i++)
+    {
+        printf("\n Element at position %d: %d",i,hash[i]);
+    }
+    cout<<endl;
+
+    return 0;
+}
